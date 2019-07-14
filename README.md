0197c8f50e24a342cf77debca65837e65bad3c80
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-07-14 16:20:28 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# simple-keyboard-swipe-keyboard-npm
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import Keyboard from "simple-keyboard";
+import swipe from "swipe-keyboard";
+
+// CSS
+import "simple-keyboard/build/css/index.css";
+import "./index.css";
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  useMouseEvents: true,
+  modules: [swipe]
+});
+
+/**
+ * Update simple-keyboard when input is changed directly
+ */
+document.querySelector(".input").addEventListener("input", event => {
+  keyboard.setInput(event.target.value);
+});
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+
+  /**
+   * If you want to handle the shift and caps lock buttons
+   */
+  if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
